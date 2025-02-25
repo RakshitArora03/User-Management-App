9d9c20c523deba0e5c73a4fdfeaa8ca36d705323
Rakshit Arora <arorarakshit6688@gmail.com>
2025-02-26 00:19:01 +0530

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        config.externals.push("bcrypt")
-      }
-      return config
-    },
-  }
-  
- 
-  
-  
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("bcrypt");
+    }
+    return config;
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : "http://localhost:3000",
+  },
+};
+
 export default nextConfig;
